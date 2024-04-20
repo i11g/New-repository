75a1116d5dae56dac6d0d059ec48a43b05a40ab6
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-04-20 18:13:17 +0300

    files added

---
+++ Story Spoil Testing/StorySpoilTesting.cs
@@ -0,0 +1,16 @@
+namespace Story_Spoil_Testing
+{
+    public class StorySpoilTesting
+    {
+        [OneTimeSetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
