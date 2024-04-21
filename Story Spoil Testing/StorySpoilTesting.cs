bc870899679863f93cecd94c44a9aa2d3be7e74a
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-04-21 06:22:09 +0300

    files added

--- Story Spoil Testing/StorySpoilTesting.cs
+++ Story Spoil Testing/StorySpoilTesting.cs
@@ -1,10 +1,29 @@
+using RestSharp;
+using RestSharp.Authenticators;
+
 namespace Story_Spoil_Testing
 {
     public class StorySpoilTesting
     {
+        private RestClient client;
+
         [OneTimeSetUp]
         public void Setup()
         {
+            string jwtTokwn = GetJwtToken("iv111","123456");
+
+            var options = new RestClientOptions(" ")
+            {
+                Authenticator = new JwtAuthenticator("")
+            };
+                        
+            this.client = new RestClient(options);
+        }
+
+        private string GetJwtToken(string username, string password)
+        {
+            RestClient tempClient = new RestClient(" ");
+
         }
 
         [Test]
